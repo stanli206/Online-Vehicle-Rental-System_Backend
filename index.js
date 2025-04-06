f77968f9ec1d62ba00c7ee90d94eb7d5d7ea3fdc
stanli206 <stantilin@gmail.com>
2025-04-07 01:19:33 +0530

    `Update CORS origin

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://onlinerentauto.netlify.app",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
