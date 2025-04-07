d76fcb2932bf6da4d94d61a96c224821680a2580
stanli206 <stantilin@gmail.com>
2025-04-07 11:00:35 +0530

    "Removed auth from /success POST route in paymentRoutes.js"

--- Routers/paymentRoutes.js
+++ Routers/paymentRoutes.js
@@ -9,7 +9,7 @@ import { authMiddleware } from "../Middleware/authMiddleware.js";
 const router = express.Router();
 
 router.post("/createPayment", authMiddleware, createPayment);
-router.post("/success", authMiddleware, updatePaymentStatus);
+router.post("/success", updatePaymentStatus);
 router.get("/invoice/:bookingId", authMiddleware, getInvoiceDetailsByBookingId);
 
 export default router;
